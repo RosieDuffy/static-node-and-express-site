303e23d080449bbd69fb871e16805c8385957557
RosieDuffy <rose.j.duffy@gmail.com>
2023-10-23 17:47:11 +0100

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # static-node-and-express-site
- Treehouse Project 6 - Static Node.js and Express Site
+
+Treehouse Project 6 - Static Node.js and Express Site
+
+-- Personal Portfolio Project --
+
+Personal portfolio created using Node and Express.
+Homepage includes thumbnails for 5 completed projects that can be clicked on. A project page is then created dynamically displaying more information on the site including further screenshots and links to the live sites and GitHub repos.
+About page displaying more information on me and links to social media.
+
+-- Personalised changes to css --
+
+Box shadows added to project thumbnails on the landing page.
+Navigation links changed to orange.
+Side bar background colour changed.
+Thumbnail in sidebar circular instead of rectangular.
+Font changed throughout.
+Bottom margin added to project thumbnails to give a little more space.
+Text in sidebar and underneath project thumbnails centered.
--- app.js
+++ app.js
@@ -11,19 +11,27 @@ app.set("view engine", "pug");
 
 app.use("/static", express.static("public"));
 
+// Route to homepage - rendering index pug view with access to projects variable
+
 app.get("/", (req, res) => {
   res.render("index", { projects });
 });
 
+// Route to about page
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
+// Route to project pages. Number in url is used to match with project ID from the data. Project view is rendered with the project available as a local
 
 app.get("/project/:id", (req, res) => {
   const projectID = req.params.id;
   const project = projects.find(({ id }) => id === parseInt(projectID));
   res.render("project", { project });
 });
+
+// 404 Error Handler //
 
 app.use((req, res, next) => {
   const err = new Error("Page not found");
@@ -32,6 +40,8 @@ app.use((req, res, next) => {
   console.log(err);
   res.render("page-not-found", { err });
 });
+
+// Global Error Handler //
 
 app.use((err, req, res, next) => {
   if (err.message && err.status) {
